042b4bdac2f1d772ab240aece1c236a5b4cd7dd6
deon86116 <deon86116@gmail.com>
2020-06-03 16:33:25 +0800

    ll

--- exam03/main.cpp
+++ exam03/main.cpp
@@ -1,5 +1,9 @@
 #include "mbed.h"
 
+#include "MQTTNetwork.h"
+#include "MQTTmbed.h"
+#include "MQTTClient.h"
+
 #include "fsl_port.h"
 
 #include "fsl_gpio.h"
@@ -82,9 +86,88 @@ void xbee_rx(void);
 void reply_messange(char *xbee_reply, char *messange);
 
 void check_addr(char *xbee_reply, char *messenger);
+WiFiInterface *wifi;
+InterruptIn butt(SW2);
+volatile bool publish = false;
+volatile int message_num = 0;
 
+void messageArrived(MQTT::MessageData& md) {
+   MQTT::Message &message = md.message;
+   char msg[300];
+   sprintf(msg, "Message arrived: QoS%d, retained %d, dup %d, packetID %d\r\n", message.qos, message.retained, message.dup, message.id);
+   printf(msg);
+   wait_ms(1000);
+   char payload[300];
+   sprintf(payload, "Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
+   printf(payload);
+}
+
+void publish_message() {
+   publish = true;
+}
+butt.rise(&publish_message);
+   uint8_t who_am_i, D[2], res[6];
+
+   int16_t acc16;
+
+   float t[3];
+
+
+   // Enable the FXOS8700Q
+
+
+   FXOS8700CQ_readRegs( FXOS8700Q_CTRL_REG1, &D[1], 1);
+
+   D[1] |= 0x01;
+
+   D[0] = FXOS8700Q_CTRL_REG1;
+
+   FXOS8700CQ_writeRegs(D, 2);
+
+void publish1(float v){
+   while (true) {
+      if (publish) {
+            message_num++;
+            MQTT::Message message;
+            char buff[100];
+            sprintf(buff,"V=%1.4f\r\n",v);
+
+            message.qos = MQTT::QOS0;
+            message.retained = false;
+            message.dup = false;
+            message.payload = (void*) buff;
+            message.payloadlen = strlen(buff) + 1;
+            rc = client.publish(topic, message);
+
+            printf("rc:  %d\r\n", rc);
+            printf("Puslish message: %s\r\n", buff);
+            client.yield(10);
+            wait(0.5);
+
+      }
+   }
+}
 
 int main(){
+
+    wifi = WiFiInterface::get_default_instance();
+   if (!wifi) {
+      printf("ERROR: No WiFiInterface found.\r\n");
+      return -1;
+   }
+
+
+   printf("\nConnecting to %s...\r\n", MBED_CONF_APP_WIFI_SSID);
+   int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
+   if (ret != 0) {
+      printf("\nConnection error: %d\r\n", ret);
+      return -1;
+   }
+
+   
+   NetworkInterface* net = wifi;
+   MQTTNetwork mqttNetwork(net);
+   MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
 
   pc.baud(9600);
 
@@ -237,6 +320,8 @@ void getAcc(Arguments *in, Reply *out) {
 
     float v = ((((t1[0])*0.1)^(2))+(((t1[1])*0.1)^(2)))^(1/2)
    xbee.printf("V=%1.4f \r\n",v);
+   publish1(v);
+
 
 }
 
